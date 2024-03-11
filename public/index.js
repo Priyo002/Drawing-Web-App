0672c3750c0d67f6c759ec4cef7f284cd33a8787
Priyabrata Das <priyobapi@gmail.com>
2024-03-11 23:16:05 +0530

    Updating

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
-
+const localurl="http://localhost:3000"
+const awsurl="http://ec2-15-206-170-218.ap-south-1.compute.amazonaws.com"
          
 document.getElementById('Ckey').addEventListener('click',async ()=>{
-    const resp = await fetch(process.env.awsurl+"/createKey",{
+    const resp = await fetch(awsurl+"/createKey",{
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -10,7 +11,7 @@ document.getElementById('Ckey').addEventListener('click',async ()=>{
     const temp = await resp.json();
     const str = String(temp.key);
     console.log(temp.key);
-    window.open(process.env.awsurl + "/" + str);
+    window.open(awsurl + "/" + str);
 })
 document.getElementById('btt').addEventListener('click',()=>{
     const t = document.getElementById('txtt').value;
