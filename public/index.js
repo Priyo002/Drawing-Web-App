7d517684adcedfea12b1ded060a68ddedbfff30f
Priyabrata Das <priyobapi@gmail.com>
2024-04-07 12:38:04 +0530

    Modifying

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const localurl="http://localhost:3000"
 const awsurl="https://www.drawhub.online"
          
 document.getElementById('Ckey').addEventListener('click',async ()=>{
-    const resp = await fetch(localurl+"/createKey",{
+    const resp = await fetch(awsurl+"/createKey",{
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -11,9 +11,9 @@ document.getElementById('Ckey').addEventListener('click',async ()=>{
     const temp = await resp.json();
     const str = String(temp.key);
     //console.log(temp.key);
-    window.open(localurl + "/" + str);
+    window.open(awsurl + "/" + str);
 })
 document.getElementById('btt').addEventListener('click',()=>{
     const t = document.getElementById('txtt').value;
-    window.open(localurl + "/" + t);
+    window.open(awsurl + "/" + t);
 })
