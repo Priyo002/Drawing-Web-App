45a56d709747c2f358421b9506e886005fbc89a4
Priyabrata Das <priyobapi@gmail.com>
2024-05-04 09:45:30 +0530

    testing

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
+app.get('/hello',(req,res)=>{
+    res.send('Working');
+})
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname+'/public/index.html'));
 })
